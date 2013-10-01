0a3f50e941fccc13290829bae0fad936c0bdb823
fellowapeman <box@up.edu>
2013-10-01 11:18:21 -0700

    Update version.php

--- version.php
+++ version.php
@@ -1,3 +1,21 @@
 <?php
 
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 $plugin->version = 2013050814;
+$plugin->requires  = 2011070100; // Requires Moodle 2.1. See http://docs.moodle.org/dev/Moodle_Versions
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.0';
