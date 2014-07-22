e0e67522a5d948bc94388fd191f473b03e9e7250
Syxton <davidso1@rose-hulman.edu>
2014-07-22 15:43:07 -0400

    IMPROVEMENT: Make the setting text match
    The text of the two new settings were not well formatted.

--- lang/en/block_my_courses.php
+++ lang/en/block_my_courses.php
@@ -1,6 +1,6 @@
 <?php
-// $Id: block_my_courses.php,v 1.2 2008-09-03 09:44:03 moodler Exp $ 
-// block_my_courses.php 
+// $Id: block_my_courses.php,v 1.2 2008-09-03 09:44:03 moodler Exp $
+// block_my_courses.php
 
 $string['pluginname'] = 'My Courses Menu';
 $string['course'] = 'Course';
@@ -8,10 +8,10 @@ $string['course'] = 'Course';
 $string['my_courses:addinstance'] = 'The ability to add on Site page';
 $string['my_courses:myaddinstance'] = 'The ability to add on the My page';
 
-$string['enablelastviewed'] = 'Show last viewed courses';
+$string['enablelastviewed'] = 'Last viewed courses section';
 $string['configenablelastviewed'] = 'Adds a section that displays the last courses the user has visited.';
 $string['lastviewedamount'] = 'Last viewed amount';
-$string['configlastviewedamount'] = 'Adjusts the amount of courses that will be displayed in the last viewed area.';
-$string['enablefavorites'] = 'Enable the Favorites section';
+$string['configlastviewedamount'] = 'Adjusts the maximum amount of courses to be displayed in the last viewed section.';
+$string['enablefavorites'] = 'Favorite courses section';
 $string['configenablefavorites'] = 'Adds a section that displays the users selected favorites.';
 ?>
