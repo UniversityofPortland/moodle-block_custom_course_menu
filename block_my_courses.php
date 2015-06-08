8612935bff1aaff3e24f231eba19d4121e51d2e0
upats <box@up.edu>
2015-06-08 11:42:55 -0700

    added boilerplates
    getting prepped for submission to moodle plugins database

--- block_my_courses.php
+++ block_my_courses.php
@@ -1,4 +1,27 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Main set up of my_courses block
+ *
+ * @package    block-my_courses
+ * @category   blocks
+ * @copyright  2015 University of Porltand
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
 
 class block_my_courses extends block_base {
     function init() {
@@ -15,9 +38,9 @@ class block_my_courses extends block_base {
         if ($CFG->version < 20150500) { // If not using Moodle 2.9+
             $PAGE->requires->js('/blocks/my_courses/js/jquery-1.9.1.min.js');
         	$PAGE->requires->js('/blocks/my_courses/js/ui/js/jquery-ui-1.10.3.min.js');
-            $PAGE->requires->js('/blocks/my_courses/js/courses.js');    	
+            $PAGE->requires->js('/blocks/my_courses/js/courses.js');
         } else { // If using Moodle 2.9+ use the requirejs version and built in jQuery and UI
-            $PAGE->requires->js('/blocks/my_courses/js/courses.js');    
+            $PAGE->requires->js('/blocks/my_courses/js/courses.js');
         }
 
         $this->content = new stdClass;
@@ -42,8 +65,8 @@ class block_my_courses extends block_base {
             $footer .= html_writer::link($url, html_writer::tag('div', get_string('fulllistofcourses') . '...', array("style" => "text-align:center;")));
         }
 
-        if(!empty($CFG->block_my_courses_showsearch) && 
-            ($CFG->block_my_courses_showsearch == 1 || 
+        if(!empty($CFG->block_my_courses_showsearch) &&
+            ($CFG->block_my_courses_showsearch == 1 ||
             ($CFG->block_my_courses_showsearch == "admin" && (is_siteadmin($USER->id) || has_capability('moodle/cohort:manage', context_system::instance(), $USER->id))))) {
             $strsearchcourses= get_string("search");
             $searchurl = new moodle_url('/course/search.php');
--- interface.php
+++ interface.php
@@ -1,4 +1,27 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Builds the main interface view of the block
+ *
+ * @package    block-my_courses
+ * @category   blocks
+ * @copyright  2015 University of Porltand
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
 
 require_once '../../config.php';
 
@@ -281,18 +304,18 @@ function get_last_viewed() {
                 $category->name = "Last $CFG->block_my_courses_lastviewedamount Viewed";
                 $category->id = "lastviewed";
                 $category->courses = array();
-                
+
                 if (isset($category_meta["lastviewed"])) {
                     $category->meta = $category_meta["lastviewed"];
                 } else {
                     $category->meta = (object) array('hide' => 0, 'sortorder' => 1);
                 }
-                
+
                 $categories["lastviewed"] = $category;
             }
             $course->meta = (object) array('hide' => 0, 'sortorder' => $order);
             $order++;
-            $categories["lastviewed"]->courses[$course->id] = $course;           
+            $categories["lastviewed"]->courses[$course->id] = $course;
         }
 
     }
