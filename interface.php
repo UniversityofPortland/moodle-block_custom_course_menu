ad60a1ca1352463353c102af075665d3131ad084
Rachel Smith <smithra17@up.edu>
2016-01-22 08:54:56 -0800

    Updated queries in interface.php in order to get last viewed courses to load faster

--- interface.php
+++ interface.php
@@ -298,8 +298,9 @@ function get_last_viewed() {
                 c.id=l.course WHERE userid='$USER->id' AND course != 1 AND module='course' GROUP BY course) b ON
                 a.course = b.course AND a.time = b.time GROUP BY a.course ORDER BY b.time DESC LIMIT $lva)";
     } else { // Moodle 2.7+.
-        $sql = "SELECT courseid, max(timecreated) as date FROM {logstore_standard_log} WHERE userid='$USER->id' AND
-                courseid > 1 GROUP BY courseid ORDER BY `date` DESC LIMIT $lva";
+        $sql = "SELECT a.courseid, max(a.timecreated) as 'date', a.userid FROM (SELECT * FROM {logstore_standard_log}
+                WHERE courseid !=0 and courseid !=1) as a WHERE a.userid='$USER->id' GROUP BY a.courseid ORDER BY
+                'date' DESC LIMIT $lva";
     }
 
     $latestcourses = $DB->get_records_sql($sql);
@@ -307,7 +308,7 @@ function get_last_viewed() {
     $categories = array();
     $order = 1;
     foreach ($latestcourses as $latest) {
-        if ($course = $DB->get_record('course', array('id' => $latest->courseid), '*')) {
+        if ($course = get_course($latest->courseid)) {
             if (!isset($categories["lastviewed"])) {
                 $params = array('id' => "lastviewed");
                 $category = new stdClass();
