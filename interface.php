af56559d14f57b1b49c23aa77e46303e0b431106
Rachel Smith <smithra17@up.edu>
2016-01-22 10:32:32 -0800

    reverted sql query as it was not sorting courses correctly. Fixed access control for userid

--- interface.php
+++ interface.php
@@ -24,7 +24,6 @@
 
 require_once('../../config.php');
 
-
 $PAGE->set_context(context_system::instance());
 
 $editing = optional_param("editing", 0, PARAM_INT);
@@ -289,7 +288,6 @@ function sort_my_categories($categories) {
  */
 function get_last_viewed() {
     global $CFG, $DB, $USER;
-
     $categorymeta = get_meta_for('category');
     $coursemeta = get_meta_for('course');
     $lva = get_config('block_custom_course_menu')->lastviewedamount;
@@ -298,9 +296,8 @@ function get_last_viewed() {
                 c.id=l.course WHERE userid='$USER->id' AND course != 1 AND module='course' GROUP BY course) b ON
                 a.course = b.course AND a.time = b.time GROUP BY a.course ORDER BY b.time DESC LIMIT $lva)";
     } else { // Moodle 2.7+.
-        $sql = "SELECT a.courseid, max(a.timecreated) as 'date', a.userid FROM (SELECT * FROM {logstore_standard_log}
-                WHERE courseid !=0 and courseid !=1) as a WHERE a.userid='$USER->id' GROUP BY a.courseid ORDER BY
-                'date' DESC LIMIT $lva";
+        $sql = "SELECT courseid, max(timecreated) as date FROM {logstore_standard_log} WHERE userid='$USER->id' AND
+                courseid > 1 GROUP BY courseid ORDER BY `date` DESC LIMIT $lva";
     }
 
     $latestcourses = $DB->get_records_sql($sql);
